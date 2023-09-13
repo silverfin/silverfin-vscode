61a6b8e6597050e3fda17fc5c2a42a16068114b8
Wouter Bruynsteen <99254741+Woetfin@users.noreply.github.com>
2023-09-13 13:56:28 +0200

    update language configuration + indentation rules + add script to generate language config json (#29)

---
+++ languages/constants.js
@@ -0,0 +1,35 @@
+// These HTML elements do not require to be closed (either via </tag> or <tag />)
+exports.voidElements = [
+  "area",
+  "base",
+  "br",
+  "col",
+  "embed",
+  "hr",
+  "img",
+  "input",
+  "keygen",
+  "link",
+  "menuitem",
+  "meta",
+  "param",
+  "source",
+  "track",
+  "wbr",
+];
+
+exports.openingLiquidTags = [
+  "if",
+  "unless",
+  "capture",
+  "comment",
+  "case",
+  "when",
+  "for",
+  "fori",
+  "adjustmentbutton",
+  "addnewinputs",
+  "stripnewlines",
+  "locale",
+  "currencyconfiguration",
+];
