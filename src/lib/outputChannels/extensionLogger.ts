528ff4d8190be12dc5f5f6e0419c14d3bff067c4
Agustin <agustin.rufino@silverfin.com>
2024-02-10 11:15:33 +0100

    Create new Logger classes to replace outputChannels
    - Singleton Classes to make sure that we can only have one instance of each
    - No need to pass them in every constructor method
    - ExtensionLogger and UserLogger

---
+++ src/lib/outputChannels/extensionLogger.ts
@@ -0,0 +1,47 @@
+import * as vscode from "vscode";
+
+/**
+ * This class is responsible for creating an output channel for the extension logs.
+ * It is a singleton class, meaning that only one instance of it can be created.
+ * This class is used to log the extension's activities and errors.
+ * Initialize it by calling the `plug` method.
+ * Use the `log` method to log messages.
+ */
+export default class ExtensionLogger {
+  private static uniqueInstance: ExtensionLogger | null = null;
+  outputChannel: vscode.OutputChannel;
+
+  private constructor() {
+    this.outputChannel = vscode.window.createOutputChannel(
+      "Silverfin (Extension Logs)"
+    );
+  }
+
+  public static plug(): ExtensionLogger {
+    if (!ExtensionLogger.uniqueInstance) {
+      ExtensionLogger.uniqueInstance = new ExtensionLogger();
+    }
+    return ExtensionLogger.uniqueInstance;
+  }
+
+  /**
+   * @param args - The message to log. Can be a string or an object.
+   * Logs the message to the output channel.
+   * If the message is an object, it will be stringified.
+   * The message will be prefixed with the name of the class that called the log method.
+   */
+  public log(...args: (string | any)[]): void {
+    let message = args
+      .map((arg) => {
+        if (typeof arg === "string") {
+          return arg;
+        } else {
+          return JSON.stringify(arg);
+        }
+      })
+      .join(" || ");
+    const callingClassName = this.constructor.name;
+    message = `[${callingClassName}] ${message}`;
+    this.outputChannel.appendLine(message);
+  }
+}
---
+++ src/lib/outputChannels/userLogger.ts
@@ -0,0 +1,32 @@
+import * as vscode from "vscode";
+
+/**
+ * This class is responsible for creating an output channel destinated to the User
+ * It is a singleton class, meaning that only one instance of it can be created.
+ * This class is used to log output relevant to the user.
+ * Initialize it by calling the `plug` method.
+ * Use the `log` method to log messages.
+ */
+export default class UserLogger {
+  private static uniqueInstance: UserLogger | null = null;
+  outputChannel: vscode.OutputChannel;
+
+  private constructor() {
+    this.outputChannel = vscode.window.createOutputChannel("Silverfin (Users)");
+  }
+
+  public static plug(): UserLogger {
+    if (!UserLogger.uniqueInstance) {
+      UserLogger.uniqueInstance = new UserLogger();
+    }
+    return UserLogger.uniqueInstance;
+  }
+
+  /**
+   * @param message - The message to log.
+   * Logs the message to the output channel. The message is intented to be read by the user.
+   */
+  public log(message: string): void {
+    this.outputChannel.appendLine(message);
+  }
+}
